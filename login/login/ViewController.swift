a5565e7b87867ae1eb8b05fb9878516a98febaac
Michael Apolonio <michaelugale626@gmail.com>
2020-02-05 21:14:39 +0800

    created login screen

--- login/login/ViewController.swift
+++ login/login/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
