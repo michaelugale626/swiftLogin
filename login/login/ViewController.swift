b82ebb924b5d6417904a634b91c009a762abd072
Michael Apolonio <michaelugale626@gmail.com>
2020-02-05 20:34:19 +0800

    initial setup

---
+++ login/login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  login
+//
+//  Created by Michael Apolonio B. Ugale on 5/2/20.
+//  Copyright © 2020 sgcarmart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
